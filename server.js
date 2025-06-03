0f7d8b9f0b1c0bbe1093c06837c90fd3d486bf46
scientificoderhash <freelancergpt123@gmail.com>
2025-06-03 14:08:30 +0530

    Backend complete

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let data = [];
-let localUrl = 'http://localhost:3000/';
+let localUrl = 'https://hk-azfn.onrender.com/';
 
 app.post('/shorten', (req,res) => {
 
--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -15,7 +15,7 @@ function InputForm({ setShortUrl, setIsLoading, setError }) {
     setError('')
     
     try {
-      const response = await fetch('http://localhost:3000/shorten', {
+      const response = await fetch('https://hk-azfn.onrender.com/shorten', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url })
